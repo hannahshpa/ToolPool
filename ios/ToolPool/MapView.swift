0422119cf5d0cdace551032bd825d58a76c84e11
Giovanni <giovmoya@gmail.com>
2020-10-30 00:42:38 -0700

    prototype for map working. added location manager file for storing user location information from phone. coordinator file allows user interaction. mapview file renders map.

---
+++ ios/ToolPool/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  ToolPool
+//
+//  Created by Giovanni Moya on 10/30/20.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}

a35c0c2217134726e41ca312f79e025b2e46dc07
Hannah Park <hannahpark@Hannahs-MBP.lan>
2020-11-06 12:42:45 -0800

    updates

--- ToolPool/SearchView.swift
+++ ToolPool/SearchView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct SearchView: View {
     var body: some View {
-        Text("Search")
+        NavigationView {
+            Form {
+                Section {
+                    Text("Hello World")
+                }
+            }
+            .navigationBarTitle(Text("Find Tools", displayMode:.center))
+        }
     }
 }
 

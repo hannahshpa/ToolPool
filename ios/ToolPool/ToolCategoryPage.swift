b77e011b83aae86ac65602df5c6989b6256e77a1
Hannah Park <hannahpark@Hannahs-MBP.lan>
2020-11-08 16:19:38 -0800

    adding tools categories basic functionality

---
+++ ios/ToolPool/ToolCategoryPage.swift
@@ -0,0 +1,20 @@
+//
+//  ToolCategoryPage.swift
+//  ToolPool
+//
+//  Created by Hannah Park on 11/8/20.
+//
+
+import SwiftUI
+
+struct ToolCategoryPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ToolCategoryPage_Previews: PreviewProvider {
+    static var previews: some View {
+        ToolCategoryPage()
+    }
+}

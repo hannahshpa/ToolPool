464c23d0406ee9094ddacaa4acff5c343e4b2746
Hannah Park <hannahpark@Hannahs-MBP.lan>
2020-11-11 11:12:25 -0800

    Adding temporary placeholder listings for Hand Tools Category

---
+++ ios/ToolPool/FilterView.swift
@@ -0,0 +1,20 @@
+//
+//  FilterView.swift
+//  ToolPool
+//
+//  Created by Hannah Park on 11/8/20.
+//
+
+import SwiftUI
+
+struct FilterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FilterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FilterView()
+    }
+}

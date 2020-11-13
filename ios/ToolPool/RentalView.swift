b745e5b4d15c31430dd934f3a29684e83018eaea
alissamcnerney <49324647+alissamcnerney@users.noreply.github.com>
2020-11-13 00:39:45 -0800

    basic rental pages

--- ios/ToolPool/RentalView.swift
+++ ios/ToolPool/RentalView.swift
@@ -7,9 +7,54 @@
 
 import SwiftUI
 
+
 struct RentalView: View {
     var body: some View {
-      Text("Rental")
+        NavigationView {
+            List{
+                //usually fetch list of rentals with this user id
+                //may have to makes seperate queries for rentals based on time/approval
+                //for each rental make navlink passing in rental obj
+                Section(header: Text("Pending Rentals")) //may need to make sections link to own pages if there are too many rentals
+                {
+                    NavigationLink(destination: ManagePendingResPage(toolName: "tool1")) {
+                        Text("tool1")
+                    }
+                    NavigationLink(destination: ManagePendingResPage(toolName: "tool1")) {
+                        Text("tool2")
+                    }
+                    NavigationLink(destination: ManagePendingResPage(toolName: "tool1")) {
+                        Text("tool3")
+                    }
+                }
+                Section(header: Text("Upcoming Rentals"))
+                {
+                    NavigationLink(destination: ManageUpcomingResPage(toolName: "tool1")) {
+                        Text("tool4")
+                    }
+                    NavigationLink(destination: ManageUpcomingResPage(toolName: "tool1")) {
+                        Text("tool5")
+                    }
+                    NavigationLink(destination: ManageUpcomingResPage(toolName: "tool1")) {
+                        Text("tool6")
+                    }
+                }
+                Section(header: Text("Past Rentals"))
+                {
+                    NavigationLink(destination: ManagePastResPage(toolName: "tool1")) {
+                        Text("tool7")
+                    }
+                    NavigationLink(destination: ManagePastResPage(toolName: "tool1")) {
+                        Text("tool8")
+                    }
+                    NavigationLink(destination: ManagePastResPage(toolName: "tool1")) {
+                        Text("tool9")
+                    }
+                }
+            }
+            .navigationBarTitle("My Rentals", displayMode: .large)
+            .navigationBarBackButtonHidden(true)
+        }
     }
 }
 

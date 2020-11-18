fbcfdc2312c79218868eb8c0e2a96b95540ce4ef
Hannah Park <hannahpark@Hannahs-MBP.lan>
2020-11-17 17:14:03 -0800

    Add tool details to tool listing page

---
+++ ios/ToolPool/MakeReservationView.swift
@@ -0,0 +1,20 @@
+//
+//  MakeReservationView.swift
+//  ToolPool
+//
+//  Created by Hannah Park on 11/17/20.
+//
+
+import SwiftUI
+
+struct MakeReservationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MakeReservationView_Previews: PreviewProvider {
+    static var previews: some View {
+        MakeReservationView()
+    }
+}

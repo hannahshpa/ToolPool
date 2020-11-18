7615c11dadaa58f9c8d99e0c326b6df78ed493c3
Hannah Park <hannahpark@Hannahs-MBP.lan>
2020-11-18 14:25:39 -0800

    Create Make Reservation view

--- ios/ToolPool/MakeReservationView.swift
+++ ios/ToolPool/MakeReservationView.swift
@@ -8,13 +8,44 @@
 import SwiftUI
 
 struct MakeReservationView: View {
+    let date: Date
+    @State var selectedDate = Date()
+    init(date: Date) {
+        self.date = date
+        self._selectedDate = State(initialValue: date)
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Form {
+                Section {
+                    DatePicker("Start Date", selection: $selectedDate, displayedComponents: .date)
+                }
+                Section {
+                    DatePicker("Start Time", selection: $selectedDate, displayedComponents: .hourAndMinute)
+                }
+                Section {
+                    DatePicker("End Date", selection: $selectedDate, displayedComponents: .date)
+
+                }
+                Section {
+                    DatePicker("End Time", selection: $selectedDate, displayedComponents: .hourAndMinute)
+                }
+            }
+            NavigationLink(destination:SearchView()) { // todo: change to make reservation
+                Text("Make Reservation")
+                    .frame(minWidth:0, maxWidth:300)
+                    .background(Color.orange)
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .cornerRadius(40)
+            }
+        }
+        .navigationTitle("Reservation Details")
     }
 }
 
 struct MakeReservationView_Previews: PreviewProvider {
     static var previews: some View {
-        MakeReservationView()
+        MakeReservationView(date:Date())
     }
 }

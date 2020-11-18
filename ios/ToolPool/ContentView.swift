6156b4a9dcc6c636ff2b2234d4a782da2476c808
Giovanni <giovmoya@gmail.com>
2020-11-18 15:59:28 -0800

    new working prototype for map feature. Need to connect tool look up with DB. also remove search feature

--- ios/ToolPool/ContentView.swift
+++ ios/ToolPool/ContentView.swift
@@ -6,28 +6,65 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
-    var body: some View {
-        TabView {
-            SearchView()
-                .tabItem {
-                    Image(systemName: "magnifyingglass")
-                    Text("Search")
+    
+    @ObservedObject var locationManager = LocationManager()
+    @State private var tools: [Tool] = [Tool]()
+    @State private var search: String = ""
+    
+    private func getNearByTools() {
+        
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = search
+        
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            if let response = response {
+                //array of mkitems
+                let mapItems = response.mapItems
+                self.tools = mapItems.map {
+                    Tool(placemark: $0.placemark)
                 }
-            RentalView()
-                .tabItem {
-                    Image(systemName: "wrench") // or toolbox "briefcase"
-                    Text("Rentals")
-                }
-
-            ProfileView()
-                .tabItem {
-                    Image(systemName: "person")
-                    Text("Profile")
-                }
+            }
         }
     }
+
+    var body: some View {
+        ZStack(alignment: .top) {
+            
+            MapView(tools: tools)
+            
+            TextField("Search For Tool", text: $search, onEditingChanged: {
+                        _ in })
+            {
+                self.getNearByTools()
+            }.textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .offset(y: 44)
+        }
+    }
+//    var body: some View {
+//        TabView {
+//            SearchView()
+//                .tabItem {
+//                    Image(systemName: "magnifyingglass")
+//                    Text("Search")
+//                }
+//            RentalView()
+//                .tabItem {
+//                    Image(systemName: "wrench") // or toolbox "briefcase"
+//                    Text("Rentals")
+//                }
+//
+//            ProfileView()
+//                .tabItem {
+//                    Image(systemName: "person")
+//                    Text("Profile")
+//                }
+//        }
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
--- ios/ToolPool/MapView.swift
+++ ios/ToolPool/MapView.swift
@@ -4,17 +4,60 @@
 //
 //  Created by Giovanni Moya on 10/30/20.
 //
-
+import Foundation
 import SwiftUI
+import MapKit
 
-struct MapView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class Coordinator: NSObject, MKMapViewDelegate {
+    var control: MapView
+    init(_ control: MapView) {
+        self.control = control
+    }
+    
+    func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+        
+        if let annotationView = views.first {
+            if let annotation = annotationView.annotation {
+                if annotation is MKUserLocation {
+                    let region = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 1000,
+                                                    longitudinalMeters: 1000)
+                    mapView.setRegion(region, animated: true)
+                }
+            }
+        }
     }
 }
 
-struct MapView_Previews: PreviewProvider {
-    static var previews: some View {
-        MapView()
+struct MapView: UIViewRepresentable {
+    
+    let tools: [Tool]
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let map = MKMapView()
+        map.showsUserLocation = true
+        map.delegate = context.coordinator
+        return map
     }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        //
+        updateAnnotation(from: uiView)
+    }
+    
+    private func updateAnnotation(from mapView: MKMapView) {
+        mapView.removeAnnotations(mapView.annotations)
+        let annotations = self.tools.map(ToolAnnotation.init)
+        mapView.addAnnotations(annotations)
+    }
+
 }
+//
+//struct MapView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MapView()
+//    }
+//}

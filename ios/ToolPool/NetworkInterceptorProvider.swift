9729e7112b99d5b134a4ba05e137a1a9c3b2b0ea
Casey <casey.olsen@gmail.com>
2020-11-21 11:11:40 -0800

    tried adding token inceptor to network

---
+++ ios/ToolPool/NetworkInterceptorProvider.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInterceptorProvider.swift
+//  ToolPool
+//
+//  Created by Olsen on 11/20/20.
+//
+
+import Foundation
+import Apollo
+
+class NetworkInterceptorProvider: LegacyInterceptorProvider {
+    override func interceptors<Operation: GraphQLOperation>(for operation: Operation) -> [ApolloInterceptor] {
+        var interceptors = super.interceptors(for: operation)
+        interceptors.insert(TokenAddingInterceptor(), at: 0)
+        return interceptors
+    }
+}

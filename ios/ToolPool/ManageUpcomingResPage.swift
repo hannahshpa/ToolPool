9587be7819f3ac5845dd808c93103b7eec67dfa5
alissamcnerney <49324647+alissamcnerney@users.noreply.github.com>
2020-12-02 16:30:40 -0800

    all fields working in borrows pages

--- ios/ToolPool/ManageUpcomingResPage.swift
+++ ios/ToolPool/ManageUpcomingResPage.swift
@@ -15,17 +15,24 @@ struct ManageUpcomingResPage: View {
         GeometryReader {
             geometry in
           VStack {
-            Image("tool")
+            if (loadImage(fileName: String(borrow.tool.id)) != nil) {
+                Image(uiImage: loadImage(fileName: String(borrow.tool.id))!)
                 .resizable()
                 .frame(width: geometry.size.width, height: geometry.size.width, alignment: .center)
                 .aspectRatio(contentMode: .fit)
+            } else {
+              Image("tool")
+                .resizable()
+                .frame(width: geometry.size.width, height: geometry.size.width, alignment: .center)
+                .aspectRatio(contentMode: .fit)
+            }
             Text(borrow.tool.name)
                 .font(.title)
                 .foregroundColor(.black)
             Divider()
             Group {
-                //Text("Start: \(NSDate(timeIntervalSinceReferenceDate: TimeInterval(borrow.loanPeriod.start)!) )")
-                //Text("End: \(NSDate(timeIntervalSinceReferenceDate: TimeInterval(borrow.loanPeriod.end)!) )")
+                Text("Start: \(NSDate(timeIntervalSinceReferenceDate: TimeInterval(borrow.loanPeriod.start)))")
+                Text("End: \(NSDate(timeIntervalSinceReferenceDate: TimeInterval(borrow.loanPeriod.end)))")
                 Text("Cost: \(borrow.cost)")
                 Text("Location: (insert map)")
                 Text("Owner: " + borrow.tool.owner.name)

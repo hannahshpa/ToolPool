19ff4e98e3737df60b9a615d15e3ff70109df48a
Giovanni <giovmoya@gmail.com>
2020-12-03 23:59:38 -0800

    added UI tests for map view features

--- ios/ToolPoolUITests/MapViewTest.swift
+++ ios/ToolPoolUITests/MapViewTest.swift
@@ -85,24 +85,63 @@ class MapViewTest: XCTestCase {
         wait(for: [expectation], timeout: 10.0)
     }
 
-    func testExample() throws {
+    func getDirectionsToToolTest() throws {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        
-        let app = XCUIApplication()
+        // if app doesn't crash at the end then apple maps did not return a nil
+        let app2 = XCUIApplication()
+        let app = app2
         app.buttons["Sign In"].tap()
         
-        let tablesQuery = app.tables
-        tablesQuery/*@START_MENU_TOKEN@*/.textFields["Username"]/*[[".cells[\"Username\"].textFields[\"Username\"]",".textFields[\"Username\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.textFields["Password"]/*[[".cells[\"Password\"].textFields[\"Password\"]",".textFields[\"Password\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let tablesQuery = app2.tables
+        let emailTextField = tablesQuery/*@START_MENU_TOKEN@*/.textFields["Email"]/*[[".cells[\"Email\"].textFields[\"Email\"]",".textFields[\"Email\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        emailTextField.tap()
+        emailTextField.tap()
+        emailTextField.typeText("joe.bruin@ucla.edu")
+   
+        
+        let passwordTextField = tablesQuery/*@START_MENU_TOKEN@*/.textFields["Password"]/*[[".cells[\"Password\"].textFields[\"Password\"]",".textFields[\"Password\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        passwordTextField.tap()
+        passwordTextField.tap()
+        passwordTextField.typeText("password")
+
         app.buttons["Enter"].tap()
+        app.buttons["Add New Tool"].tap()
+        
+        let toolNameTextField = tablesQuery/*@START_MENU_TOKEN@*/.textFields["Tool Name"]/*[[".cells[\"Tool Name\"].textFields[\"Tool Name\"]",".textFields[\"Tool Name\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        toolNameTextField.tap()
+        toolNameTextField.tap()
+        toolNameTextField.typeText("Tent2")
+        
+        let costPerHourTextField = tablesQuery/*@START_MENU_TOKEN@*/.textFields["Cost Per Hour"]/*[[".cells[\"Cost Per Hour\"].textFields[\"Cost Per Hour\"]",".textFields[\"Cost Per Hour\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        costPerHourTextField.tap()
+        costPerHourTextField.tap()
+        costPerHourTextField.typeText("15")
+        
+        let descriptionTextField = tablesQuery/*@START_MENU_TOKEN@*/.textFields["Description"]/*[[".cells[\"Description\"].textFields[\"Description\"]",".textFields[\"Description\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        descriptionTextField.tap()
+        descriptionTextField.tap()
+        descriptionTextField.typeText("Medium sized steel hammer")
+        descriptionTextField.typeText("\n")
+        
+        tablesQuery/*@START_MENU_TOKEN@*/.buttons["Category"]/*[[".cells[\"Category\"].buttons[\"Category\"]",".buttons[\"Category\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.buttons["Camping"]/*[[".cells[\"Camping\"].buttons[\"Camping\"]",".buttons[\"Camping\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+
+        tablesQuery/*@START_MENU_TOKEN@*/.buttons["Condition"]/*[[".cells[\"Condition\"].buttons[\"Condition\"]",".buttons[\"Condition\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let tablesQuery2 = app.tables
+        tablesQuery2.cells["Good"].children(matching: .other).element(boundBy: 0).tap()
+        tablesQuery2.staticTexts["IMAGES"]/*@START_MENU_TOKEN@*/.swipeRight()/*[[".swipeUp()",".swipeRight()"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        tablesQuery/*@START_MENU_TOKEN@*/.buttons["Photo library"]/*[[".cells[\"Photo library\"].buttons[\"Photo library\"]",".buttons[\"Photo library\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.scrollViews.otherElements.images["Photo, March 12, 2011, 4:17 PM"].tap()
+        app/*@START_MENU_TOKEN@*/.tables.containing(.cell, identifier:"Category").element/*[[".tables.containing(.cell, identifier:\"Photo library\").element",".tables.containing(.cell, identifier:\"Good\").element",".tables.containing(.cell, identifier:\"Category\").element"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.buttons["Submit Tool"].swipeUp()
         app.tabBars["Tab Bar"].buttons["magnifyingglass"].tap()
-        //test that device found user location
-        testGetLocation()
-        app/*@START_MENU_TOKEN@*/.buttons["Camping"]/*[[".scrollViews.buttons[\"Camping\"]",".buttons[\"Camping\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app/*@START_MENU_TOKEN@*/.buttons["Power chisel"]/*[[".scrollViews.buttons[\"Power chisel\"]",".buttons[\"Power chisel\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app/*@START_MENU_TOKEN@*/.buttons["Get Directions To Tool"]/*[[".scrollViews.buttons[\"Get Directions To Tool\"]",".buttons[\"Get Directions To Tool\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        //check tha
+        app2/*@START_MENU_TOKEN@*/.buttons["Camping"]/*[[".scrollViews.buttons[\"Camping\"]",".buttons[\"Camping\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app2.buttons["Tent2"].tap()
+                app2/*@START_MENU_TOKEN@*/.buttons["Get Directions To Tool"]/*[[".scrollViews.buttons[\"Get Directions To Tool\"]",".buttons[\"Get Directions To Tool\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+
     }
 
 }
